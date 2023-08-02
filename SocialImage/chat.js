d4d9e0d14775948a75f32cf5af9789877352afeb
Bard of Code <harnettmusic@gmail.com>
2023-08-02 09:10:29 -0230

    Project continues

--- SocialImage/chat.js
+++ SocialImage/chat.js
@@ -3,12 +3,36 @@ const getUser = () => {
                 .then( response => response.json() )
                 .then(user => {return user})
 }
+const updateTimes = (comment) => {
+    let time = comment.makeTimeString()
+    $(".date").text(time + " ago " + comment.user+" said ");
+}
+
+const addCount = (comment) => {
+    comment.addCount()
+    $(this).closest(".likeCount").text(comment.likeCount)
+}
 
 $( () => {
     let user = "Chris";
+    $('.user').text(user)
+    let dateString = new Date().toDateString()
+    $('.userDate').text(dateString)
         
-    $('#add_comment').on ("click", () => {       
+    $('#add_comment').on("click", () => {       
         let comment = new Comment($("#newComment").val(), user );
         comment.postComment()
-        }); 
+        setInterval(updateTimes, 1000, comment)
+        $('.newCommentClass').on("click", ".up", () => {
+            addCount(comment);
+        })
+    
+        $('.newCommentClass').on("click", ".down", () => {
+            console.log($(this).parents(".likes"))
+            $(this).parents(".likes").text("test");
+        })
+        });
+    
     });
+
+    
